0f2bc651e0702734e3ea4d82ef8eb0592930399d
Musca Mihail <muscamihaill@gmail.com>
2024-05-11 23:39:52 +0300

    Changed horizontal navbar to vertical navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,39 +1,24 @@
 import Link from 'next/link';
 
+function NavButton({ href, text } : { href: string, text: string }) {
+    return (
+        <li className="my-6">
+            <Link href={href}>
+                <p className="hover:underline text-3xl">{text}</p>
+            </Link>
+        </li>
+    );
+}
+
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 w-full bg-gray-800 text-white p-4">
-            <ul className="flex justify-center">
-                <li className="mx-2">
-                    <Link href="/">
-                    <p className="hover:underline">Home</p>
-                    </Link>
-                </li>
-                <li className="mx-2">
-                    <Link href="/about">
-                    <p className="hover:underline">About Me</p>
-                    </Link>
-                </li>
-                <li className="mx-2">
-                    <Link href="/projects">
-                    <p className="hover:underline">Projects</p>
-                    </Link>
-                </li>
-                <li className="mx-2">
-                    <Link href="/resume">
-                    <p className="hover:underline">Resume</p>
-                    </Link>
-                </li>
-                <li className="mx-2">
-                    <Link href="/contact">
-                    <p className="hover:underline">Contact</p>
-                    </Link>
-                </li>
-                <li className="mx-2">
-                    <Link href="/contests">
-                    <p className="hover:underline">Contests</p>
-                    </Link>
-                </li>
+        <nav className="fixed h-full bg-gray-800 text-white p-20">
+            <ul className="flex-col justify-center">
+                <NavButton href="/" text="Home" />
+                <NavButton href="/about" text="About Me" />
+                <NavButton href="/projects" text="Projects" />
+                <NavButton href="/resume" text="Resume" />
+                <NavButton href="/contact" text="Contact" />
             </ul>
         </nav>
     );
--- src/app/contests/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Contests() {
-    return (
-        <div className="mt-16">Contests Page Content</div>
-    );
-}
