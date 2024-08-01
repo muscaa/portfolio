759a8e5b0d92b85b4f679e53a33f0dd7491a7f9d
Musca Mihail <muscamihaill@gmail.com>
2024-08-01 18:22:39 +0300

    Update Loading Screen

--- src/app/components/LoadingScreen.tsx
+++ src/app/components/LoadingScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import PartyParrot from "../svg/PartyParrot";
 
 export default function LoadingScreen({ loading }: { loading: boolean }) {
