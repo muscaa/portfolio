68ece66f94a9590bea28c876ddadab35ac4c488b
Musca Mihail <muscamihaill@gmail.com>
2024-08-03 21:57:49 +0300

    Readded loading screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`${font.className}`}>
+            <body className={`${font.className} overflow-hidden`}>
                 {children}
             </body>
         </html>
