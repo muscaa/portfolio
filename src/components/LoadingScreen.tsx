ecb1e4ad988987d66554ae57b30aa0f3c1e8f735
Musca Mihail <muscamihaill@gmail.com>
2025-03-06 16:27:42 +0200

    update resumes, improve loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -6,6 +6,13 @@ import * as Tracker from "@/Tracker";
 export default function LoadingScreen({ loading }: { loading: boolean }) {
     const [visible, setVisible] = useState(true);
     useEffect(() => {
+        const animation = document.getElementById("loading-screen-animation");
+        if (animation == null) return;
+
+        animation.style.opacity = "1";
+    }, []);
+
+    useEffect(() => {
         if (loading) return;
 
         const loadingScreen = document.getElementById("loading-screen");
@@ -25,8 +32,8 @@ export default function LoadingScreen({ loading }: { loading: boolean }) {
     if (!visible) return <></>;
     return (
         <div id="loading-screen" className="fixed inset-0 transition-all duration-500 ease-in-out bg-background-1 z-50">
-            <div className="flex flex-col w-full h-full justify-center items-center">
-                <PartyParrot className="w-64 h-64" />
+            <div id="loading-screen-animation" className="flex flex-col w-full h-full justify-center items-center transition-all duration-500 ease-in-out opacity-0">
+                <PartyParrot className="w-64 h-64" duration={700} />
                 <h2 className="font-bold p-16">Loading...</h2>
             </div>
         </div>
