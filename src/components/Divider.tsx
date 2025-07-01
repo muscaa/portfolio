6abd15a9d7d3d2cb00343a82f83b5d4c8228fa2d
Musca Mihail <muscamihaill@gmail.com>
2025-07-01 20:56:04 +0300

    remove next project

--- src/components/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Divider({ className }: { className?: string }) {
-    return (
-        <div className={`h-px bg-text-2 ${className}`}></div>
-    );
-}
--- src/components/ProfileIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Image from "next/image";
-
-export default function ProfileIcon({ className }: { className?: string }) {
-    return (
-        <Image src="profile_64x64.png" width={32} height={32} alt="Profile" className={`rounded-full ${className}`} />
-    );
-}
