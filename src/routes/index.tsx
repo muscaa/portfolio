e17e25b5979a96e92aa278557c04d635fa762a5b
Musca Mihail <muscamihaill@gmail.com>
2025-07-02 13:54:42 +0300

    init qwik ui with tailwind v4 fix, add qwik ui button, add temporary theme, add test buttons

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,32 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui";
 
 export default component$(() => {
   return (
     <>
       <h1>Hi 👋</h1>
-      <div>
+      <div class="flex flex-col gap-2">
         Can't wait to see what you build with qwik!
-        <br />
         Happy coding.
+        <Button look="alert">
+            alert
+        </Button>
+        <Button look="ghost">
+            ghost
+        </Button>
+        <Button look="link">
+            link
+        </Button>
+        <Button look="outline">
+            outline
+        </Button>
+        <Button look="primary">
+            primary
+        </Button>
+        <Button look="secondary">
+            secondary
+        </Button>
       </div>
     </>
   );
