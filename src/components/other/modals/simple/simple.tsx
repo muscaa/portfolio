9db795c1f36d02195bc31cd8e2cdfc7ef34b0615
Musca Mihail <muscamihaill@gmail.com>
2025-10-10 19:00:28 +0300

    refactor: added modal overlay & popup, fix modal panel, fix simple modal

--- src/components/other/modals/simple/simple.tsx
+++ src/components/other/modals/simple/simple.tsx
@@ -2,7 +2,6 @@ import {
     Slot,
     Signal,
     component$,
-    useContext,
 } from "@builder.io/qwik";
 import { cn } from "@qwik-ui/utils";
 import {
@@ -25,75 +24,43 @@ export const ModalTrigger = component$<ButtonProps>(({ size, look, ...props }) =
     );
 });
 
-interface SimpleModalContentProps {
+interface SimpleModalProps {
+    "bind:show"?: Signal<boolean>;
     title?: string;
     description?: string;
     class?: string;
 }
 
-const SimpleModalContent = component$<SimpleModalContentProps>((props) => {
-    const context = useContext(Modal.contextId);
-
-    return (
-        <>
-            <Slot name="trigger" />
-            <div
-                data-open={context.showSig.value && ""}
-                data-closed={!context.showSig.value && ""}
-                class={cn(
-                    "fixed z-50 left-0 top-0 w-screen h-screen",
-                    "flex flex-col p-2 justify-center items-center",
-                    "data-[closed]:hidden data-[open]:visible"
-                )}
-            >
-                <Modal.Panel
-                    class={cn("relative flex flex-col gap-2 m-0", props.class)}
-                >
-                    {
-                        props.title && (
-                            <Modal.Title>
-                                {props.title}
-                            </Modal.Title>
-                        )
-                    }
-                    {
-                        props.description && (
-                            <Modal.Description>
-                                {props.description}
-                            </Modal.Description>
-                        )
-                    }
-                    <Separator />
-                    <Slot />
-                    <Modal.Close
-                        class={cn(
-                            buttonVariants({ size: "icon", look: "link" }),
-                            "absolute top-2 right-2",
-                        )}
-                    >
-                        <Lu.X class="h-6 w-6" />
-                    </Modal.Close>
-                </Modal.Panel>
-            </div>
-        </>
-    );
-});
-
-interface SimpleModalProps extends SimpleModalContentProps {
-    "bind:show"?: Signal<boolean>;
-}
-
 export const SimpleModal = component$<SimpleModalProps>(({ "bind:show": bindShow, ...props }) => {
     return (
         <Modal.Root bind:show={bindShow}>
-            <SimpleModalContent
-                {...props}
-            >
-                <q:template q:slot="trigger">
-                    <Slot name="trigger" />
-                </q:template>
+            <Slot name="trigger" />
+            <Modal.Panel>
+                {
+                    props.title && (
+                        <Modal.Title>
+                            {props.title}
+                        </Modal.Title>
+                    )
+                }
+                {
+                    props.description && (
+                        <Modal.Description>
+                            {props.description}
+                        </Modal.Description>
+                    )
+                }
+                <Separator />
                 <Slot />
-            </SimpleModalContent>
+                <Modal.Close
+                    class={cn(
+                        buttonVariants({ size: "icon", look: "link" }),
+                        "absolute top-2 right-2",
+                    )}
+                >
+                    <Lu.X class="h-6 w-6" />
+                </Modal.Close>
+            </Modal.Panel>
         </Modal.Root>
     );
 });
--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -9,9 +9,9 @@ const Trigger = HeadlessModal.Trigger;
 
 const Close = HeadlessModal.Close;
 
-export const panelVariants = cva(
+export const overlayVariants = cva(
     [
-        "fixed bg-background p-6 text-foreground transition-all",
+        "fixed z-50 size-full group flex flex-col bg-transparent text-foreground transition-all",
         "backdrop:brightness-50 backdrop:backdrop-blur-sm",
         "data-[open]:duration-300 data-[closing]:duration-300",
         "data-[open]:animate-in data-[closing]:animate-out",
@@ -22,11 +22,11 @@ export const panelVariants = cva(
     {
         variants: {
             position: {
-                center: "max-w-lg w-lg m-auto rounded-xl shadow-lg data-[closed]:fade-out data-[open]:fade-in data-[closed]:zoom-out-95 data-[open]:zoom-in-95 data-[open]:slide-in-from-bottom-2",
-                top: "inset-x-0 top-0 mt-0 mb-auto w-full rounded-b-xl border-b data-[closing]:slide-out-to-top data-[open]:slide-in-from-top",
-                bottom: "inset-x-0 bottom-0 mb-0 mt-auto w-full rounded-t-xl border-t data-[closing]:slide-out-to-bottom data-[open]:slide-in-from-bottom",
-                left: "inset-y-0 left-0 ml-0 mr-auto h-full max-w-sm rounded-r-xl border-r data-[closing]:slide-out-to-left data-[open]:slide-in-from-left",
-                right: "inset-y-0 right-0 mr-0 ml-auto h-full max-w-sm rounded-l-xl border-l data-[closing]:slide-out-to-right data-[open]:slide-in-from-right",
+                center: "justify-center items-center p-2",
+                top: "justify-start items-center",
+                bottom: "justify-end items-center",
+                left: "justify-center items-start",
+                right: "justify-center items-end",
             },
         },
         defaultVariants: {
@@ -35,17 +35,71 @@ export const panelVariants = cva(
     },
 );
 
-type PanelProps = PropsOf<typeof HeadlessModal.Panel> &
-    VariantProps<typeof panelVariants>;
+type OverlayProps = PropsOf<typeof HeadlessModal.Panel> &
+    VariantProps<typeof overlayVariants>;
 
-const Panel = component$<PanelProps>(({ position, ...props }) => {
+const Overlay = component$<OverlayProps>(({ position, ...props }) => {
     return (
         <HeadlessModal.Panel
             {...props}
-            class={cn(panelVariants({ position }), props.class)}
+            bypassCloseOnBackdropClick
+            class={cn(overlayVariants({ position }), props.class)}
         >
             <Slot />
         </HeadlessModal.Panel>
+    );
+});
+
+export const popupVariants = cva(
+    [
+        "relative flex flex-col max-w-full max-h-full gap-2 bg-background p-6 text-foreground transition-all",
+        "group-data-[open]:duration-300 group-data-[closing]:duration-300",
+        "group-data-[open]:animate-in group-data-[closing]:animate-out",
+    ],
+    {
+        variants: {
+            position: {
+                center: "max-w-lg w-full rounded-xl shadow-lg group-data-[closed]:fade-out group-data-[open]:fade-in group-data-[closed]:zoom-out-95 group-data-[open]:zoom-in-95 group-data-[open]:slide-in-from-bottom-2",
+                top: "w-full rounded-b-xl border-b group-data-[closing]:slide-out-to-top group-data-[open]:slide-in-from-top",
+                bottom: "w-full rounded-t-xl border-t group-data-[closing]:slide-out-to-bottom group-data-[open]:slide-in-from-bottom",
+                left: "h-full max-w-sm rounded-r-xl border-r group-data-[closing]:slide-out-to-left group-data-[open]:slide-in-from-left",
+                right: "h-full max-w-sm rounded-l-xl border-l group-data-[closing]:slide-out-to-right group-data-[open]:slide-in-from-right",
+            },
+        },
+        defaultVariants: {
+            position: "center",
+        },
+    },
+);
+
+type PopupProps = PropsOf<"div"> &
+    VariantProps<typeof popupVariants>;
+
+const Popup = component$<PopupProps>(({ position, ...props }) => {
+    return (
+        <div
+            {...props}
+            class={cn(popupVariants({ position }), props.class)}
+        >
+            <Slot />
+        </div>
+    );
+});
+
+type PanelProps = PopupProps;
+
+const Panel = component$<PanelProps>(({ position, ...props }) => {
+    return (
+        <Overlay
+            position={position}
+        >
+            <Popup
+                {...props}
+                position={position}
+            >
+                <Slot />
+            </Popup>
+        </Overlay>
     );
 });
 
@@ -71,14 +125,13 @@ const Description = component$<PropsOf<"p">>(({ ...props }) => {
     );
 });
 
-const contextId = HeadlessModal.contextId;
-
 export const Modal = {
     Root,
     Trigger,
     Close,
+    Overlay,
+    Popup,
     Panel,
     Title,
     Description,
-    contextId,
 };
