0d5c0e78e10a51c3b3f8349206e30ea7a7fd4bc0
musca <49593385+muscaa@users.noreply.github.com>
2026-03-05 19:09:27 +0200

    Update basePath and assetPrefix for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? "https://muscaa.github.io/portfolio/" : "",
+    //basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+    //assetPrefix: process.env.NODE_ENV === "production" ? "https://muscaa.github.io/portfolio/" : "",
+    basePath: process.env.NODE_ENV === "production" ? "" : "",
+    assetPrefix: process.env.NODE_ENV === "production" ? "https://musca.dev/" : "",
     images: {
         unoptimized: true,
     },
